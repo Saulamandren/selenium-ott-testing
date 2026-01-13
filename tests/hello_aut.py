bbadca8a6dbadf9ed7a56438520856b93239ea57
Saulamandren <salamandren104@gmail.com>
2026-01-13 17:12:02 +0700

    Fix AUT test with Selenium explicit wait

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,52 +1,45 @@
 import unittest, sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class AutTest(unittest.TestCase):
 
-    # Method ini dijalankan sebelum setiap test
     def setUp(self):
-        # Konfigurasi Firefox
         options = webdriver.FirefoxOptions()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
 
-        # Alamat Selenium Hub
         server = "http://localhost:4444"
 
-        # Menghubungkan Selenium ke Firefox di container
         self.browser = webdriver.Remote(
             command_executor=server,
             options=options
         )
 
-        # Menutup browser otomatis setelah test selesai
         self.addCleanup(self.browser.quit)
 
-    # Test untuk halaman utama AUT
     def test_homepage(self):
 
-        # Jika ada parameter URL dari command line, gunakan itu
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
-            # Default URL (AUT)
-            url = "http://localhost"
+            url = "http://docker-apache"
 
-        # Membuka halaman AUT
         self.browser.get(url)
 
-        # Hasil yang diharapkan muncul di halaman
         expected_result = "Welcome back, Guest!"
 
-        # Mengambil elemen <p> dari halaman
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p')
+        # Tunggu sampai elemen <p> muncul
+        wait = WebDriverWait(self.browser, 10)
+        element = wait.until(
+            EC.presence_of_element_located((By.TAG_NAME, "p"))
+        )
 
-        # Validasi apakah teks sesuai dengan yang diharapkan
-        self.assertIn(expected_result, actual_result.text)
+        self.assertIn(expected_result, element.text)
 
 
-# Menjalankan test jika file dieksekusi langsung
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
