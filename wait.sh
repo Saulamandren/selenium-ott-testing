f300e10fda5cf01e9d73fca0c170be32bbb423cf
Saulamandren <salamandren104@gmail.com>
2026-01-13 16:05:27 +0700

    Stabilize Selenium startup and wait.sh on GitHub Actions

--- wait.sh
+++ wait.sh
@@ -2,6 +2,7 @@
 
 echo "Waiting for Selenium Hub to be ready..."
 
+# Tunggu sampai Selenium menyatakan "ready": true
 until curl -s http://localhost:4444/wd/hub/status | grep '"ready":true'; do
   echo "Selenium is not ready yet..."
   sleep 2
