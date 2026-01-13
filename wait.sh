2efb077ec6dd05ab52f5e76cd6b728b99ecce0a0
Saulamandren <salamandren104@gmail.com>
2026-01-13 16:42:12 +0700

    Fix Selenium wait using UI readiness

--- wait.sh
+++ wait.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "Waiting for Selenium Hub to be ready..."
+echo "Waiting for Selenium Hub UI..."
 
-# Tunggu sampai Selenium menyatakan "ready": true
-until curl -s http://localhost:4444/wd/hub/status | grep '"ready":true'; do
-  echo "Selenium is not ready yet..."
-  sleep 2
+until curl -s http://localhost:4444/ui > /dev/null; do
+  echo "Selenium UI not ready yet..."
+  sleep 3
 done
 
-echo "Selenium Hub is ready!"
+echo "Selenium Hub UI is ready!"
