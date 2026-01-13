f6f79b20201493a155c075502d8e4494c1250343
Saulamandren <salamandren104@gmail.com>
2026-01-13 16:04:02 +0700

    Fix Selenium wait and Linux compatibility

--- wait.sh
+++ wait.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Menentukan bahwa file ini adalah script bash
 
-# Loop sampai Selenium Hub bisa diakses di port 4444
-until $(curl --output /dev/null --silent --head --fail http://localhost:4444); do
-    # Menampilkan pesan bahwa Selenium Hub masih belum siap
-    echo "waiting for selenium hub being started"
+echo "Waiting for Selenium Hub to be ready..."
 
-    # Menunggu 1 detik sebelum mencoba lagi
-    sleep 1
+until curl -s http://localhost:4444/wd/hub/status | grep '"ready":true'; do
+  echo "Selenium is not ready yet..."
+  sleep 2
 done
+
+echo "Selenium Hub is ready!"
